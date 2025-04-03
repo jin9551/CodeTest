94b7435cb85f914f929f003db8eddc10fbcc2d5d
Jin Ha Jeong <wlsgk9551@gmail.com>
2025-04-03 22:02:27 +0900

    Time: 1 ms (6.46%), Space: 41 MB (43.54%) - LeetHub

---
+++ 2619-categorize-box-according-to-criteria/2619-categorize-box-according-to-criteria.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String categorizeBox(int length, int width, int height, int mass) {
+        // Bulky 
+        // 아무 한변의 길이가 10^4을 넘어서면 
+        // 볼륨이 10^9을 넘어서면
+
+        // Heavy 
+        // 박스의 질량이 100을 넘어서면
+
+        // 박스가 Heavy하면서 Bulky하면 Both
+
+        // 박스가 둘 다 만족하지 못하면 Neither
+
+        // 박스가 Bulky만 만족하면 Bulky
+        // 박스가 Heavy만 만족하면 Heavy
+        boolean Bulky = false;
+        boolean Heavy = false;
+
+        long volume = (long)length * (long)width * (long)height;
+        System.out.println(volume);
+        if(length >= 10000 || width>= 10000 || height>= 10000 || volume >= 1000000000 ){
+            Bulky = true;
+        } 
+
+        if(mass >= 100){
+            Heavy = true;
+        }
+
+        if(Bulky && Heavy){
+            return "Both";
+        } else if (Bulky && !Heavy){
+            return "Bulky";
+        } else if(Heavy && !Bulky) {
+            return "Heavy";
+        } else {
+            return "Neither";
+        }
+
+    }
+}
