2bd6918de3b244181676ed2fbc5ffb651b850722
Jin Ha Jeong <wlsgk9551@gmail.com>
2025-04-04 21:28:01 +0900

    Time: 5 ms (52.85%), Space: 45 MB (36.01%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,24 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
+        Map<Character, Integer> maps = new HashMap<>();
+        maps.put('I', 1);
+        maps.put('V', 5);
+        maps.put('X', 10);
+        maps.put('L', 50);
+        maps.put('C', 100);
+        maps.put('D', 500);
+        maps.put('M', 1000);
 
         int sum = 0;
-        for(int i=0;i<s.length();i++){
-            // 로마 숫자는 앞에서부터 큰 수가 이어져 온다.
-            // 만약 현재 숫자가 뒤의 숫자보다 작다면
-            // IV(4 = 5-1) 나 IX(9 = 10-1)처럼 특이한 케이스다.
-            if(i<s.length()-1 &&  map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
-                sum -= map.get(s.charAt(i));
+        for(int i=0; i<s.length(); i++){
+            int cur = maps.get(s.charAt(i));
+            // 현재 값이 이전 값 보다 크다면
+            if(i<s.length()-1 && cur<maps.get(s.charAt(i+1))){
+                // sum에서 이번 값을 뺀다. 
+                sum -= cur;
             } else {
-                sum += map.get(s.charAt(i));
+                // 아니라면 현재 값을 더 해준다.
+                sum += cur;
             }
 
         }
