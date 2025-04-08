d64058f584f2e99b82536d10f0260bbeed5abca0
Jin Ha Jeong <wlsgk9551@gmail.com>
2025-04-08 22:31:56 +0900

    Time: 392 ms (82.74%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,4 @@
 /* Write your PL/SQL query statement below */
 select MAX(salary) as "SecondHighestSalary"
 from employee
-where salary not in (select max(salary) from employee);
+where salary < (select max(salary) from employee);
