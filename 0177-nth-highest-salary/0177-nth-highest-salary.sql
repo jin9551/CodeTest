8f9d4ad689048a7ada22d0f3a22264aa7d155b94
Jin Ha Jeong <wlsgk9551@gmail.com>
2025-04-08 22:39:03 +0900

    Time: 496 ms (87.78%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N IN NUMBER) RETURN NUMBER IS
+result NUMBER;
+BEGIN
+    /* Write your PL/SQL query statement below */
+SELECT  DISTINCT salary  INTO result FROM 
+(SELECT salary,DENSE_RANK() OVER (ORDER BY salary DESC) R 
+FROM  Employee) WHERE R = N;
+RETURN result;
+EXCEPTION
+WHEN NO_DATA_FOUND THEN
+RETURN NULL;
+END;
