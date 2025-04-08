2860f4a28f103332a0cda68a26556f5ad8deb4ba
Jin Ha Jeong <wlsgk9551@gmail.com>
2025-04-08 11:31:32 +0900

    Time: 107 ms (40.36%), Space: 46.7 MB (10.8%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -10,9 +10,9 @@ class Solution {
 
      boolean[][] dp = new boolean[n][n];
 
-     for(int i=0; i<n;++i){
+     for(int i=0; i<n;i++){
         dp[i][i] = true;
-        for(int j=0;j<i;++j){
+        for(int j=0;j<i;j++){
             if(s.charAt(i)==s.charAt(j) && (i-j<=2 || dp[j+1][i-1])){
                 dp[j][i] = true;
                 if(i-j+1>maxLen){
