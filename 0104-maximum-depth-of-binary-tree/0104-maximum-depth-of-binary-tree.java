a31b127cbf4d8fd607b95dfb080bbbf8d75333d5
Jin Ha Jeong <wlsgk9551@gmail.com>
2025-04-10 22:56:17 +0900

    Time: 0 ms (100%), Space: 42.9 MB (26.43%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,29 +15,42 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }
 
-        Queue<TreeNode> queue = new LinkedList<>();
+        // BFS 알고리즘
+        // if(root == null) {
+        //     return 0;
+        // }
+
+        // Queue<TreeNode> queue = new LinkedList<>();
+
+        // int depth = 0;
+
+        // queue.offer(root);
+
+        // while(!queue.isEmpty()){
+        //     depth += 1;
+        //     int q_size = queue.size();
+
+        //     for(int i=0; i< q_size; i++){
+        //         TreeNode node = queue.poll();
+        //         if(node.left != null)
+        //             queue.offer(node.left);
+        //         if(node.right != null)
+        //             queue.offer(node.right);
+        //     }
+        // }
+
+        // return depth;
 
         int depth = 0;
-
-        queue.offer(root);
-
-        while(!queue.isEmpty()){
-            depth += 1;
-            int q_size = queue.size();
-
-            for(int i=0; i< q_size; i++){
-                TreeNode node = queue.poll();
-                if(node.left != null)
-                    queue.offer(node.left);
-                if(node.right != null)
-                    queue.offer(node.right);
-            }
+        if(root == null){
+            return 0;
         }
+        int leftdepth = maxDepth(root.left);
+        int rightdepth = maxDepth(root.right);
 
+        depth = Math.max(leftdepth,rightdepth)+1;
         return depth;
+
     }
 }
