d9776bb30d61af0df6ce5492e9ea5bd50032ed17
Jin Ha Jeong <wlsgk9551@gmail.com>
2025-04-12 21:43:42 +0900

    Time: 0 ms (100%), Space: 44.4 MB (29.11%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,45 @@
+import java.util.*;
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        Set<List<Integer>> s = new HashSet<>();
-        List<List<Integer>> output = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int k = j + 1;
-                int l = nums.length - 1;
-                while (k < l) {
-                    long sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-                    if (sum == target) {
-                        s.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
-                        k++;
-                        l--;
-                    } else if (sum < target) {
-                        k++;
-                    } else {
-                        l--;
+
+return new AbstractList<List<Integer>>(){
+            private List<List<Integer>> result;
+            int n=nums.length;
+
+            public List<Integer> get(int i){
+                init();
+                return result.get(i);
+            }
+
+            public int size(){
+                init();
+                return result.size();
+            }
+
+            public void init(){
+                if(result!=null) return;
+
+                Arrays.sort(nums);
+                result=new ArrayList<>();
+
+                for(int i=0;i<n-3;i++){
+                    if(i>0 && nums[i]==nums[i-1]) continue;
+                    for(int j=i+1;j<n-2;j++){
+                        if(j>i+1 && nums[j]==nums[j-1]) continue;
+                        int l=j+1, r=n-1;
+                        while(l<r){
+                            long s=(long)nums[i]+nums[j]+nums[l]+nums[r];
+                            if(s==target){
+                                result.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
+                                do l++; while(l<r && nums[l]==nums[l-1]);
+                                do r--; while(l<r && nums[r]==nums[r+1]);
+                            }else if(s<target)
+                            l++;
+                            else r--;
+                        }
                     }
                 }
             }
-        }
-        output.addAll(s);
-        return output;
+        };
     }
 }
