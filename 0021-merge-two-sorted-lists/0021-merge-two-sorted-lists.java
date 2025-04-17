7c53bad0072531945df13188a56014bfb250189f
Jin Ha Jeong <wlsgk9551@gmail.com>
2025-04-17 20:05:05 +0900

    Time: 0 ms (100%), Space: 42.6 MB (59.34%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,27 +10,41 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // 시작 포인터를 유지하기 위한 노드
-        ListNode node = new ListNode();
-        // 시작포인터를 current에 복사해서 이걸로 작업
-        ListNode current = node;
+        // // 시작 포인터를 유지하기 위한 노드
+        // ListNode node = new ListNode();
+        // // 시작포인터를 current에 복사해서 이걸로 작업
+        // ListNode current = node;
 
-        // 리스트 노드 둘 중에 하나가 끝날때까지
-        while(list1 != null && list2 != null){
-            if(list1.val > list2.val){
-                current.next = list2;
-                list2 = list2.next;
-            } else {
-                current.next = list1;
-                list1 = list1.next;
+        // // 리스트 노드 둘 중에 하나가 끝날때까지
+        // while(list1 != null && list2 != null){
+        //     if(list1.val > list2.val){
+        //         current.next = list2;
+        //         list2 = list2.next;
+        //     } else {
+        //         current.next = list1;
+        //         list1 = list1.next;
+        //     }
+        //     current = current.next;
+        // }
+
+        // // 먼저 끝난 리스트노드를 판단해서 끝나지 않은 리스트 노드로 연결
+        // current.next= (list1 != null) ? list1 : list2;
+
+        // return node.next;
+
+        if(list1!=null && list2!=null){
+        if(list1.val<list2.val){
+            list1.next=mergeTwoLists(list1.next,list2);
+            return list1;
             }
-            current = current.next;
+            else{
+                list2.next=mergeTwoLists(list1,list2.next);
+                return list2;
         }
-
-        // 먼저 끝난 리스트노드를 판단해서 끝나지 않은 리스트 노드로 연결
-        current.next= (list1 != null) ? list1 : list2;
-
-        return node.next;
+        }
+        if(list1==null)
+            return list2;
+        return list1;
     }
     
 }
